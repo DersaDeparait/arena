44fb7ac5c87e41039c9236459631b7ca956153a1
NoOneZero <turdecarta@gmail.com>
2021-02-28 21:39:46 +0200

    Перероблено до робочого виду минулого дня

--- Character.py
+++ Character.py
@@ -1,55 +1,91 @@
+import pandas
+
 class Character:
+    regular_characters = []
     params = {
         "сила": 10,
         "виносливість": 10,
         "ловкість": 10,
-        "ініціативність": 10,
+        "точність": 10,
+
         "володіння зброєю": 10,
+        "ініціативність": 10,
         "сприйняття": 10,
         "удача": 10,
     }
-    def __init__(self, name="Warrior", image=None, power=10, endurance=10, dexterity=10, initiative=10, weapon_skill=10,
-                 perception=10, luck=10,
-                 weapon=[2, 10]):
+    def __init__(self,id=0,  name="Warrior", image=None, health=1000,
+                 params_points = 10, skill_points = 3,
+                 weapon_down=10, weapon_up=20,
+                 power=10, endurance=10, dexterity=10, precision = 10,
+                 initiative=10, weapon_skill=10, perception=10, luck=10,
+
+                 block_chance=1, dodge_chance=1, counterattack_chance=1, counterattack_value=400, critical_chance=1, critical_mod=500,
+                 venom_chance=0, venom_value=0, selfheal_chance=0, selfheal_value=0,
+                 stun_chance=0,double_attack_chance=0,triple_attack_chance=0,instant_kill_chance=0,
+                 fighting_spirit_chance = 1,
+                 skills = [], **kwargs):
+
+        if Character.regular_characters == []:
+            [Character.regular_characters.append(Character.return_character(i)) for i in range(12)]
+
+        self.id = id
+        self.name = name
+        self.image = image
+        self.health_max = health
+        self.health_curent = health
+
+        self.params_points = params_points
+        self.skill_points = skill_points
+
+        self.weapon = [weapon_down, weapon_up]
+
         self.params = dict(Character.params)
         self.params["сила"] = power
         self.params["виносливість"] = endurance
         self.params["ловкість"] = dexterity
-        self.params["ініціативність"] = initiative
+        self.params["точність"] = precision
         self.params["володіння зброєю"] = weapon_skill
+        self.params["ініціативність"] = initiative
         self.params["сприйняття"] = perception
         self.params["удача"] = luck
 
         self.params_else = {
-            "Блок шанс": 1,
-            "Уворот шанс": 1,
-            "Контраудар шанс": 1,
-            "Сила контраудара": 400,
+            "Блок шанс": block_chance,
+            "Уворот шанс": dodge_chance,
+            "Контраудар шанс": counterattack_chance,
+            "Контраудар мод": counterattack_value,
+            "Критичний шанс": critical_chance,
+            "Критичний мод":critical_mod,
 
-            "Отрута шанс": 0,
-            "Отрута сила": 0,
-            "Отрута на собі": 0,
+            "Отрута шанс": venom_chance,
+            "Отрута сила": venom_value,
 
-            "Автохіл шанс": 0,
-            "Автохіл значення": 0,
+            "Автохіл шанс": selfheal_chance,
+            "Автохіл сила": selfheal_value,
 
-            "Оглушаючого удара шанс": 0,
-            "Двойний удар шанс": 0,
-            "Тройний удар шанс": 0,
+            "Оглушаючий удара шанс": stun_chance,
+            "Двойний удар шанс": double_attack_chance,
+            "Тройний удар шанс": triple_attack_chance,
+            "Моментальне вбивство шанс": instant_kill_chance,
 
-            "Бойовий дух шанс": 0
+            "Бойовий дух шанс": fighting_spirit_chance
         }
-        self.name = name
-        self.image = image
-        self.params_points = 10
-        self.skill_points = 2
-        self.position_on_ruler = 0
-        self.health_max = 1000
-        self.health_curent = 1000
-        self.weapon = weapon
-        self.skills = []
 
-    def get_info(self, info=0):
+        self.skills = []
+        if type(skills) == list:
+            self.skills = skills
+        elif type(skills) == str:
+            s = skills.split(" ")
+            print(s)
+            for i in s:
+                if i != '':
+                    self.skills.append(int(i))
+
+
+        self.venoms_on_me = {}
+        self.position_on_ruler = 0
+
+    def get_info(self, info=0): #fixme Переробити вивод
         s = "Герой {},    ❤️{}".format(self.name, self.health_max)
         s += "\n{0:<25}: {1:>5}-{2:<5}".format("Урон зброї", self.weapon[0], self.weapon[1])
         s += "\n"
@@ -66,7 +102,8 @@ class Character:
         s += "\n{0:<35}: {1:>10.1%}".format("Уворот шанс", self.params_else["Уворот шанс"] / 1000)
         s += "\n{0:<29}: {1:>10.1%}".format("Контраудар шанс", self.params_else["Контраудар шанс"] / 1000)
         s += "\n"
-        s += "\nЗалишилося вкласти очок: {}".format(self.params_points)
+        s += "\nЗалишилося очок параметрів: {}".format(self.params_points)
+        s += "\nЗалишилося очок навиків: {}".format(self.skill_points)
         return s
 
     def params_up(self, param):
@@ -75,7 +112,8 @@ class Character:
             self.params[param] += 3
 
     def get_picture(self):
-        return open(self.image, "rb")
+        path = "content/images/characters/"
+        return open(path+self.image, "rb")
 
     def add_skill(self, skill_number):
         if self.skill_points > 0:
@@ -84,51 +122,16 @@ class Character:
 
     @staticmethod
     def return_character(number=0):
-        path = "content/images/characters/"
-        if number == 0:
-            c = Character(name="No One", image=path+"x0.jpg", power=10, endurance=10, dexterity=10, initiative=10,
-                          weapon_skill=10, perception=10, luck=10, weapon=[30, 50])
-        elif number == 1:
-            c = Character(name="Рицар", image=path+"x1.jpg", power=10, endurance=10, dexterity=10, initiative=10,
-                          weapon_skill=10, perception=10, luck=10, weapon=[10, 50])
-        elif number == 2:
-            c = Character(name="Варвар", image=path+"x2.jpg", power=10, endurance=10, dexterity=10, initiative=10,
-                          weapon_skill=10, perception=10, luck=10, weapon=[30, 70])
-        elif number == 3:
-            c = Character(name="Воїн", image=path+"x3.jpg", power=10, endurance=10, dexterity=10, initiative=10,
-                          weapon_skill=10, perception=10, luck=10, weapon=[40, 45])
-        elif number == 4:
-            c = Character(name="Вор", image=path+"x4.jpg", power=10, endurance=10, dexterity=10, initiative=10,
-                          weapon_skill=10, perception=10, luck=10, weapon=[15, 30])
-        elif number == 5:
-            c = Character(name="Дуелянтка", image=path+"x5.jpg", power=10, endurance=10, dexterity=10, initiative=10,
-                          weapon_skill=10, perception=10, luck=10, weapon=[50, 60])
-        elif number == 6:
-            c = Character(name="Берсеркер", image=path+"x6.jpg", power=10, endurance=10, dexterity=10, initiative=10,
-                          weapon_skill=10, perception=10, luck=10, weapon=[10, 65])
-        elif number == 7:
-            c = Character(name="Мандрівник", image="x7.jpg", power=10, endurance=10, dexterity=10, initiative=10,
-                          weapon_skill=10, perception=10, luck=10, weapon=[40, 80])
-        elif number == 8:
-            c = Character(name="Піратка", image="x8.jpg", power=10, endurance=10, dexterity=10, initiative=10,
-                          weapon_skill=10, perception=10, luck=10, weapon=[10, 30])
-        elif number == 9:
-            c = Character(name="Алебардщик", image="x9.jpg", power=10, endurance=10, dexterity=10, initiative=10,
-                          weapon_skill=10, perception=10, luck=10, weapon=[60, 120])
-        elif number == 10:
-            c = Character(name="Темний воїн", image="x10.jpg", power=10, endurance=10, dexterity=10, initiative=10,
-                          weapon_skill=10, perception=10, luck=10, weapon=[40, 60])
-        elif number == 11:
-            c = Character(name="Лучниця", image="x11.jpg", power=10, endurance=10, dexterity=10, initiative=10,
-                          weapon_skill=10, perception=10, luck=10, weapon=[25, 50])
-        elif number == 12:
-            c = Character(name="Копейщиця", image="x12.jpg", power=10, endurance=10, dexterity=10, initiative=10,
-                          weapon_skill=10, perception=10, luck=10, weapon=[20, 40])
-        else:
-            c = Character(name="000", power=10, endurance=10, dexterity=10, initiative=10, weapon_skill=10,
-                          perception=10, luck=10, weapon=[10, 20])
+        c = Character(**Character.get_line_from_csv(number))
         return c
-
+    @staticmethod
+    def get_line_from_csv(line_number, file_path="content/data/character_params.csv"):
+        p = pandas.read_csv(file_path, sep=";")
+        p = p[line_number:line_number + 1].to_dict()
+        to_return = {}
+        for key in p:
+            to_return[key] = p[key][line_number]
+        return to_return
 
 skills = {
     0:["Оглушаючий удар", "", ""],
@@ -149,4 +152,6 @@ skills = {
     15:["Хілер", "", ""],
     16:["Натхненний", "", ""],
     17:["Натхненний +", "", ""],
+    18:["Стійкість","",""],
+    19:["Стійкість +","",""],
 }
