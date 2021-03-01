a0ead4110620eab1aa2cba51aa2d4c0e41f4685f
NoOneZero <turdecarta@gmail.com>
2021-03-01 02:56:01 +0200

    Додано кращу настройку Character.py

--- Character.py
+++ Character.py
@@ -1,93 +1,183 @@
 import pandas
+import copy
 
 class Character:
     regular_characters = []
     params = {
-        "сила": 10,
-        "виносливість": 10,
-        "ловкість": 10,
-        "точність": 10,
+        "power":[10, "сила", "Усиляє пряму атаку"],
+        "endurance":[10, "виносливість", "Збільшує кількість здоровя"],
 
-        "володіння зброєю": 10,
-        "ініціативність": 10,
-        "сприйняття": 10,
-        "удача": 10,
+        "dexterity":[10, "ловкість", "Збільшує шанси на двойний удар"],
+        "speed":[10, "скорость", "Збільшує частоту ходів"],
+
+        "precision": [10, "точність", "Збільшує критичний удар"],
+        "intelligence": [10, "інтелект", ""],
+
+        "spirit":[10, "дух", ""],
+        "wisdom":[10, "мудрість", ""],
+
+        "perception":[10, "сприйняття", "Збільшує ймовірності реакції на удари"],
+        "luck":[10, "удача", "Робить кращим життя взагалі"],
     }
+    skills = {
+        0: ["Оглушаючий удар", "", ""],
+        1: ["Двойний удар", "", ""],
+        2: ["Тройний удар", "", ""],
+        3: ["Вдачний", "", ""],
+        4: ["Народжений в рубашці", "", ""],
+        5: ["Отравлена зброя", "", ""],
+        6: ["Контраатакуючий", "", ""],
+        7: ["Гнучкий", "", ""],
+        8: ["Блокуючий", "", ""],
+        9: ["Лікар", "", ""],
+        10: ["Натхненний", "", ""],
+        11: ["Стійкість", "", ""],
+        12: ["Володіння збрєю", "", ""],
+    }
+
+    addition_points_from_one = 4
+
+    @staticmethod
+    def set_default_characters():
+        if Character.regular_characters == []:
+            [Character.regular_characters.append(Character.return_character(i)) for i in range(20)]
+    @staticmethod
+    def return_character(number=0):
+        c = Character(**Character.get_line_from_csv(number))
+        return c
+    @staticmethod
+    def get_line_from_csv(line_number, file_path="content/data/character_params.csv"):
+        p = pandas.read_csv(file_path, sep=";")
+        p = p[line_number:line_number + 1].to_dict()
+        to_return = {}
+        for key in p:
+            to_return[key] = p[key][line_number]
+        return to_return
+
+
     def __init__(self,id=0,  name="Warrior", image=None, health=1000,
                  params_points = 10, skill_points = 3,
-                 weapon_down=10, weapon_up=20,
-                 power=10, endurance=10, dexterity=10, precision = 10,
-                 initiative=10, weapon_skill=10, perception=10, luck=10,
+                 weapon_down=10, weapon_up=20, armor=10,
+                 power=10, endurance=10, dexterity=10, speed=10, precision=10, intelligence=10,
+                 spirit=10, wisdom=10, perception=10, luck=10,
 
                  block_chance=1, dodge_chance=1, counterattack_chance=1, counterattack_value=400, critical_chance=1, critical_mod=500,
                  venom_chance=0, venom_value=0, selfheal_chance=0, selfheal_value=0,
-                 stun_chance=0,double_attack_chance=0,triple_attack_chance=0,instant_kill_chance=0,
+                 stun_chance=0,double_attack_chance=0,triple_attack_chance=0,instant_kill_chance=0,rebuff_chance=50,rebuff_value=500,
                  fighting_spirit_chance = 1,
                  skills = [], **kwargs):
-
-        if Character.regular_characters == []:
-            [Character.regular_characters.append(Character.return_character(i)) for i in range(12)]
 
         self.id = id
         self.name = name
         self.image = image
-        self.health_max = health
-        self.health_curent = health
+        self.health_base = health
 
         self.params_points = params_points
         self.skill_points = skill_points
 
         self.weapon = [weapon_down, weapon_up]
+        self.armor = armor
 
-        self.params = dict(Character.params)
-        self.params["сила"] = power
-        self.params["виносливість"] = endurance
-        self.params["ловкість"] = dexterity
-        self.params["точність"] = precision
-        self.params["володіння зброєю"] = weapon_skill
-        self.params["ініціативність"] = initiative
-        self.params["сприйняття"] = perception
-        self.params["удача"] = luck
+        self.params = copy.deepcopy(Character.params)
+        self.params["power"][0] = power
+        self.params["endurance"][0] = endurance
+        self.params["dexterity"][0] = dexterity
+        self.params["speed"][0] = speed
+        self.params["precision"][0] = precision
+        self.params["intelligence"][0] = intelligence
+        self.params["spirit"][0] = spirit
+        self.params["wisdom"][0] = wisdom
+        self.params["perception"][0] = perception
+        self.params["luck"][0] = luck
 
         self.params_else = {
-            "Блок шанс": block_chance,
-            "Уворот шанс": dodge_chance,
-            "Контраудар шанс": counterattack_chance,
-            "Контраудар мод": counterattack_value,
-            "Критичний шанс": critical_chance,
-            "Критичний мод":critical_mod,
+            "block_chance": block_chance,
+            "dodge_chance": dodge_chance,
+            "counterattack_chance": counterattack_chance,
+            "counterattack_value": counterattack_value,
+            "critical_chance": critical_chance,
+            "critical_mod":critical_mod,
 
-            "Отрута шанс": venom_chance,
-            "Отрута сила": venom_value,
+            "venom_chance": venom_chance,
+            "venom_value": venom_value,
 
-            "Автохіл шанс": selfheal_chance,
-            "Автохіл сила": selfheal_value,
+            "selfheal_chance": selfheal_chance,
+            "selfheal_value": selfheal_value,
 
-            "Оглушаючий удара шанс": stun_chance,
-            "Двойний удар шанс": double_attack_chance,
-            "Тройний удар шанс": triple_attack_chance,
-            "Моментальне вбивство шанс": instant_kill_chance,
+            "stun_chance": stun_chance,
+            "double_attack_chance": double_attack_chance,
+            "triple_attack_chance": triple_attack_chance,
+            "instant_kill_chance": instant_kill_chance,
+            "rebuff_chance":rebuff_chance,
+            "rebuff_value": rebuff_value,
 
-            "Бойовий дух шанс": fighting_spirit_chance
+            "fighting_spirit_chance": fighting_spirit_chance
         }
 
-        self.skills = []
-        if type(skills) == list:
-            self.skills = skills
-        elif type(skills) == str:
-            s = skills.split(" ")
-            print(s)
-            for i in s:
-                if i != '':
-                    self.skills.append(int(i))
-
+        self.skills = [0] * 50
+        if type(skills) == list: self.skills = skills
 
         self.venoms_on_me = {}
         self.position_on_ruler = 0
 
+        self.calculate_upped_params()
+
+    def calculate_upped_params(self):
+        small_div = 4
+        big_mult = 20
+        upper_mult = 50
+        # 0: ["Оглушаючий удар", "", ""],
+        # 1: ["Двойний удар", "", ""],
+        # 2: ["Тройний удар", "", ""],
+        # 3: ["Вдачний", "", ""],
+        # 4: ["Народжений в рубашці", "", ""], fixme
+        # 5: ["Отравлена зброя", "", ""],
+        # 6: ["Контраатакуючий", "", ""],
+        # 7: ["Гнучкий", "", ""],
+        # 8: ["Блокуючий", "", ""],
+        # 9: ["Лікар", "", ""],
+        # 10: ["Натхненний", "", ""],
+        # 11: ["Стійкість", "", ""],
+        # 12: ["Володіння збрєю", "", ""],
+
+        self.upped_params = {
+            "health": self.health_base + self.params["endurance"][0] * big_mult,
+            "weapon": [self.weapon[0] * (1 + self.params["power"][0] / 100),
+                       self.weapon[1] * (1 + self.params["power"][1] / 100)],
+            "armor": self.armor,
+
+            "block_chance": self.params_else["block_chance"] + self.params["endurance"][0]//small_div + self.params["perception"][0]//small_div,
+            "dodge_chance": self.params_else["dodge_chance"] + self.params["dexterity"][0]//small_div + self.params["perception"][0]//small_div,
+            "counterattack_chance": self.params_else["counterattack_chance"] + self.params["intelligence"][0]//small_div + self.params["perception"][0]//small_div,
+            "counterattack_value": self.params_else["counterattack_value"] + self.params["intelligence"][0] * big_mult,
+            "critical_chance": self.params_else["critical_chance"] + self.params["precision"][0]//small_div + self.params["perception"][0]//small_div,
+            "critical_mod":self.params_else["critical_mod"] + self.params["precision"][0]*big_mult,
+
+            "venom_chance": self.params_else["venom_chance"],
+            "venom_value": self.params_else["venom_value"],
+
+            "selfheal_chance": self.params_else["selfheal_chance"] + self.params["endurance"][0]//upper_mult,
+            "selfheal_value": self.params_else["selfheal_value"] + self.params["endurance"][0]//upper_mult,
+
+            "stun_chance": self.params_else["stun_chance"] + self.skills[0] * 100,
+            "double_attack_chance": self.params_else["double_attack_chance"] + self.params["dexterity"][0]//small_div + self.skills[1] * 200,
+            "triple_attack_chance": self.params_else["triple_attack_chance"] + self.params["dexterity"][0]//small_div + self.skills[2] * 200,
+            "instant_kill_chance": self.params_else["instant_kill_chance"] + self.params["wisdom"][0]//upper_mult,
+            "rebuff_chance":self.params_else["rebuff_chance"] + self.params["speed"][0]//small_div,
+            "rebuff_value": self.params_else["rebuff_value"] + self.params["speed"][0]*big_mult,
+
+            "fighting_spirit_chance": self.params_else["fighting_spirit_chance"],
+
+            "lack": self.params["luck"] + self.skills[3] * big_mult
+        }
+
+        self.health_curent = self.upped_params["health"]
+
+
     def get_info(self, info=0): #fixme Переробити вивод
-        s = "Герой {},    ❤️{}".format(self.name, self.health_max)
-        s += "\n{0:<25}: {1:>5}-{2:<5}".format("Урон зброї", self.weapon[0], self.weapon[1])
+        s = "Герой {},    ❤️{}".format(self.name, self.upped_params["health"])
+        s += "\n{0:<25}: {1:>5}-{2:<5}".format("Урон зброї: ", self.upped_params["weapon"][0], self.upped_params["weapon"][1])
+        s += "\n{0:<25}: {1:>5}".format("Броня: ", self.upped_params["armor"])
         s += "\n"
         if info > 0:
             s += "\n{0:<30}: {1:>10}".format("сила", self.params["сила"])
@@ -109,49 +199,11 @@ class Character:
     def params_up(self, param):
         if self.params_points > 0:
             self.params_points -= 1
-            self.params[param] += 3
-
-    def get_picture(self):
-        path = "content/images/characters/"
-        return open(path+self.image, "rb")
-
+            self.params[param] += Character.addition_points_from_one
     def add_skill(self, skill_number):
         if self.skill_points > 0:
-            self.skills.append(skill_number)
+            self.skills[skill_number] += self.skills[skill_number]
             self.skill_points -= 1
-
-    @staticmethod
-    def return_character(number=0):
-        c = Character(**Character.get_line_from_csv(number))
-        return c
-    @staticmethod
-    def get_line_from_csv(line_number, file_path="content/data/character_params.csv"):
-        p = pandas.read_csv(file_path, sep=";")
-        p = p[line_number:line_number + 1].to_dict()
-        to_return = {}
-        for key in p:
-            to_return[key] = p[key][line_number]
-        return to_return
-
-skills = {
-    0:["Оглушаючий удар", "", ""],
-    1:["Оглушаючий удар +", "", ""],
-    2:["Двойний удар", "", ""],
-    3:["Тройний удар", "", ""],
-    4:["Вдачний", "", ""],
-    5:["Народжений в рубашці", "", ""],
-    6:["Отравлена зброя", "", ""],
-    7:["Отравлена зброя +", "", ""],
-    8:["Контраатакуючий", "", ""],
-    9:["Контраатакуючий +", "", ""],
-    10:["Гнучкий", "", ""],
-    11:["Гнучкий +", "", ""],
-    12:["Блокуючий", "", ""],
-    13:["Блокуючий +", "", ""],
-    14:["Лікар", "", ""],
-    15:["Хілер", "", ""],
-    16:["Натхненний", "", ""],
-    17:["Натхненний +", "", ""],
-    18:["Стійкість","",""],
-    19:["Стійкість +","",""],
-}
+    def get_picture(self):
+        path = "content/images/characters/"
+        return open(path + self.image, "rb")
--- main.py
+++ main.py
@@ -4,10 +4,8 @@ import datetime
 from Character import Character
 
 bot = telebot.TeleBot("1339981204:AAGfBHbWW4Bycellhh4cqWtgD1YAwUmF7WQ")
-
-data = {0: {"message": None, "first_name": "Rihana", "second_name": "Fenty", "character": 2,
-            "login_time": datetime.datetime.now()}, }
-
+data = {0: {"message": None, "first_name": "Rihana", "second_name": "Fenty", "character": 2, "login_time": datetime.datetime.now()}, }
+Character.set_default_characters()
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
@@ -17,101 +15,97 @@ def welcome(message):
                              "character": None,
                              "message": None}
     images = []
-    path = "content/images/characters/"
-    images.append(telebot.types.InputMediaPhoto(open(path+"x1.jpg", "rb"), caption="Рицар"))
-    images.append(telebot.types.InputMediaPhoto(open(path+"x2.jpg", "rb"), caption="Варвар"))
-    images.append(telebot.types.InputMediaPhoto(open(path+"x3.jpg", "rb"), caption="Воїн"))
-    # images.append(telebot.types.InputMediaPhoto(open(path+"x4.jpg", "rb"), caption="Вор"))
-    # images.append(telebot.types.InputMediaPhoto(open(path+"x5.jpg", "rb"), caption="Дуелянтка"))
-    # images.append(telebot.types.InputMediaPhoto(open(path+"x6.jpg", "rb"), caption="Берсеркер"))
-    # images.append(telebot.types.InputMediaPhoto(open(path+"x7.jpg", "rb"), caption="Мандрівник"))
-    # images.append(telebot.types.InputMediaPhoto(open(path+"x8.jpg", "rb"), caption="Піратка"))
-    # images.append(telebot.types.InputMediaPhoto(open(path+"x9.jpg", "rb"), caption="Алебардщик"))
-    # images.append(telebot.types.InputMediaPhoto(open(path+"x10.jpg", "rb"), caption="Темний воїн"))
-    # images.append(telebot.types.InputMediaPhoto(open(path+"x11.jpg", "rb"), caption = "Лучніца"))
-    # images.append(telebot.types.InputMediaPhoto(open(path+"x12.jpg", "rb"), caption = "Копейщіця"))
-
-    bot.send_media_group(message.chat.id, media=images)
     markup = telebot.types.InlineKeyboardMarkup()
-    markup.add(telebot.types.InlineKeyboardButton("1 - Рицар", callback_data="ch_1"))
-    markup.add(telebot.types.InlineKeyboardButton("2 - Варвар", callback_data="ch_2"))
-    markup.add(telebot.types.InlineKeyboardButton("3 - Воїн", callback_data="ch_3"))
-    markup.add(telebot.types.InlineKeyboardButton("4 - Вор", callback_data="ch_4"))
-    markup.add(telebot.types.InlineKeyboardButton("5 - Дуелянтка", callback_data="ch_5"))
-    markup.add(telebot.types.InlineKeyboardButton("6 - Берсеркер", callback_data="ch_6"))
-    markup.add(telebot.types.InlineKeyboardButton("7 - Мандрівник", callback_data="ch_7"))
-    markup.add(telebot.types.InlineKeyboardButton("8 - Піратка", callback_data="ch_8"))
-    markup.add(telebot.types.InlineKeyboardButton("9 - Алберадщик", callback_data="ch_9"))
-    markup.add(telebot.types.InlineKeyboardButton("10 - Темний воїн", callback_data="ch_10"))
-    # markup.add(telebot.types.InlineKeyboardButton("11 - Лучніца", callback_data="ch_11"))
-    # markup.add(telebot.types.InlineKeyboardButton("12 - Копейщіця", callback_data="ch_12"))
-    data[message.chat.id]["message"] = bot.send_message(message.chat.id, "Hello {}\nВиберіть персонажа: ".format(
-        message.from_user.first_name), reply_markup=markup)
+    for i in range(1, 11):
+        markup.add(telebot.types.InlineKeyboardButton(str(i) + " - " + Character.regular_characters[i].name, callback_data="ch_"+ str(i)))
+        images.append(telebot.types.InputMediaPhoto(Character.regular_characters[i].get_picture(), caption=Character.regular_characters[i].name))
+    bot.send_media_group(message.chat.id, media=images)
 
-    print(data)
-
+    data[message.chat.id]["message"] = bot.send_message(message.chat.id, "Hello {}\nВиберіть персонажа: ".format(message.from_user.first_name), reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     if "ch_" in call.data:
-        if not call.from_user.id in data:
-            data[call.from_user.id] = {"first_name": call.from_user.first_name,
-                                       "second_name": call.from_user.last_name,
-                                       "login_time": datetime.datetime.now(),
-                                       "character": None,
-                                       "message": None}
-
-        if data[call.from_user.id]["message"] != None:
-            bot.delete_message(call.from_user.id, data[call.from_user.id]["message"].message_id)
-            data[call.from_user.id]["message"] = None
-        data[call.from_user.id]["character"] = Character.return_character(int(call.data[3:]))
-
-        markup = telebot.types.InlineKeyboardMarkup()
-        for key in Character.params:
-            markup.add(
-                telebot.types.InlineKeyboardButton(key + ": " + str(data[call.from_user.id]["character"].params[key]),
-                                                   callback_data="wo_" + key))
-        data[call.from_user.id]["message"] = bot.send_photo(call.from_user.id,
-                                                            data[call.from_user.id]["character"].get_picture(),
-                                                            caption=data[call.from_user.id]["character"].get_info(),
-                                                            reply_markup=markup)
-
+        chose_character(call)
     elif "wo_" in call.data:
+        chose_params(call)
+    elif "sk_" in call.data:
         if data[call.from_user.id]["message"] != None:
             bot.delete_message(call.from_user.id, data[call.from_user.id]["message"].message_id)
             data[call.from_user.id]["message"] = None
-        data[call.from_user.id]["character"].params_up(str(call.data)[3:])
+
+        if len(call.data)>3:
+            data[call.from_user.id]["character"].add_skill(int((call.data)[3:]))
+
         markup = telebot.types.InlineKeyboardMarkup()
-        if data[call.from_user.id]["character"].params_points > 0:
-            for key in Character.params:
+        if data[call.from_user.id]["character"].skill_points > 0:
+            for key in Character.skills:
                 markup.add(telebot.types.InlineKeyboardButton(
-                    key + ": " + str(data[call.from_user.id]["character"].params[key]), callback_data="wo_" + key))
+                    str(key) + ": " + Character.skills[key][0], callback_data="sk_" + str(key)))
             data[call.from_user.id]["message"] = bot.send_photo(call.from_user.id,
                                                                 data[call.from_user.id]["character"].get_picture(),
                                                                 caption=data[call.from_user.id]["character"].get_info(),
                                                                 reply_markup=markup)
         else:
-            markup.add(telebot.types.InlineKeyboardButton("Завершити, перейти до навиків", callback_data="sk_"))
+            markup.add(telebot.types.InlineKeyboardButton("Завершити", callback_data="end"))
             data[call.from_user.id]["message"] = bot.send_photo(call.from_user.id,
                                                                 data[call.from_user.id]["character"].get_picture(),
                                                                 caption=data[call.from_user.id]["character"].get_info(
                                                                     info=1), reply_markup=markup)
-    elif "sk_" in call.data:
-        bot.send_message(call.from_user.id, "444")  # , reply_markup=markup)
     elif "end" in call.data:
         markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3, one_time_keyboard=True)
         markup.add("Weak enemie")
         markup.add("Middle enemie")
         markup.add("Strong enemie")
         bot.send_message(call.from_user.id, "Виберіть ворога", reply_markup=markup)
+def chose_character(call):
+    if not call.from_user.id in data:
+        data[call.from_user.id] = {"first_name": call.from_user.first_name,
+                                   "second_name": call.from_user.last_name,
+                                   "login_time": datetime.datetime.now(),
+                                   "character": None,
+                                   "message": None}
+
+    if data[call.from_user.id]["message"] != None:
+        bot.delete_message(call.from_user.id, data[call.from_user.id]["message"].message_id)
+        data[call.from_user.id]["message"] = None
+    data[call.from_user.id]["character"] = Character.return_character(int(call.data[3:]))
+
+    markup = telebot.types.InlineKeyboardMarkup()
+    for key in Character.params:
+        markup.add(
+            telebot.types.InlineKeyboardButton(key + ": " + str(data[call.from_user.id]["character"].params[key]),
+                                               callback_data="wo_" + key))
+    data[call.from_user.id]["message"] = bot.send_photo(call.from_user.id,
+                                                        data[call.from_user.id]["character"].get_picture(),
+                                                        caption=data[call.from_user.id]["character"].get_info(),
+                                                        reply_markup=markup)
+def chose_params(call):
+    if data[call.from_user.id]["message"] != None:
+        bot.delete_message(call.from_user.id, data[call.from_user.id]["message"].message_id)
+        data[call.from_user.id]["message"] = None
+    data[call.from_user.id]["character"].params_up(str(call.data)[3:])
+    markup = telebot.types.InlineKeyboardMarkup()
+    if data[call.from_user.id]["character"].params_points > 0:
+        for key in Character.params:
+            markup.add(telebot.types.InlineKeyboardButton(
+                key + ": " + str(data[call.from_user.id]["character"].params[key]), callback_data="wo_" + key))
+        data[call.from_user.id]["message"] = bot.send_photo(call.from_user.id,
+                                                            data[call.from_user.id]["character"].get_picture(),
+                                                            caption=data[call.from_user.id]["character"].get_info(),
+                                                            reply_markup=markup)
+    else:
+        markup.add(telebot.types.InlineKeyboardButton("Завершити, перейти до навиків", callback_data="sk_"))
+        data[call.from_user.id]["message"] = bot.send_photo(call.from_user.id,
+                                                            data[call.from_user.id]["character"].get_picture(),
+                                                            caption=data[call.from_user.id]["character"].get_info(
+                                                                info=1), reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
 def conversation(message):  # message.chat.id message.text
     if message.text == "Weak enemie":
         start_battle(message)
-
-
 def start_battle(message):
     bot.send_message(message.chat.id, "В Вашого героя: 1000 хелсів, у ворога 400 хелсів")
     health = [1000, 400]
@@ -127,6 +121,5 @@ def start_battle(message):
                 bot.send_message(message.chat.id, "Ви виграли")
             else:
                 bot.send_message(message.chat.id, "Нічя")
-
 
 bot.polling(none_stop=True)
